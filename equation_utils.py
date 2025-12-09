a9c42045090380572e34e3f2fabe282e0ebc2b7a
ahmadouda1142011-design <ahmadouda1142011@gmail.com>
2025-12-09 21:59:49 +0200

    Update equation_utils.py

--- equation_utils.py
+++ equation_utils.py
@@ -1,5 +1,6 @@
 from sympy import solve as sympy_solve
 from sympy import symbols, Eq
+from string_utils import count_atoms_in_molecule
 
 ELEMENTS = [
     'H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne',
@@ -16,6 +17,7 @@ ELEMENTS = [
     'Rg', 'Cn', 'Uut', 'Uuq', 'Uup', 'Uuh', 'Uus', 'Uuo'
 ]
 
+
 def generate_equation_for_element(compounds, coefficients, element):
     equation = 0
     for i, compound in enumerate(compounds):
@@ -26,7 +28,7 @@ def generate_equation_for_element(compounds, coefficients, element):
 
 def build_equations(reactant_atoms, product_atoms):
     reactant_coefficients = list(symbols(f'a0:{len(reactant_atoms)}'))
-    product_coefficients = list(symbols(f'b0:{len(product_atoms)}')) 
+    product_coefficients = list(symbols(f'b0:{len(product_atoms)}'))
     product_coefficients = product_coefficients[:-1] + [1]
 
     equations = []
@@ -41,14 +43,22 @@ def build_equations(reactant_atoms, product_atoms):
 
 def my_solve(equations, coefficients):
     solution = sympy_solve(equations, coefficients)
-
     if len(solution) == len(coefficients):
         coefficient_values = []
-        for coefficient in coefficients:
-            coefficient_values.append(float(solution[coefficient]))
+        for c in coefficients:
+            coefficient_values.append(float(solution[c]))
         return coefficient_values
 
 
+def balance_reaction(reaction):
+    reactants_str, products_str = reaction.split("->")
+    reactants = [r.strip() for r in reactants_str.split("+")]
+    products = [p.strip() for p in products_str.split("+")]
 
+    reactant_atoms = [count_atoms_in_molecule(r) for r in reactants]
+    product_atoms = [count_atoms_in_molecule(p) for p in products]
 
+    equations, coefficients = build_equations(reactant_atoms, product_atoms)
+    values = my_solve(equations, coefficients)
 
+    return values
